6f553112ea52a9ad09eb9c04f6e00a641fe31270
vjollca <vjollcebaxhaku@gmail.com>
2025-04-17 14:22:45 +0200

    Created feedback model

---
+++ EventOrganizer/Models/Feedback.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using EventOrganizer.Models;
+
+namespace EventOrganizer.Models
+{
+    public class Feedback
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Comment { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+
+        public int EventsId { get; set; }
+
+        public User User { get; set; }
+
+        public Events Events { get; set; }
+    }
+}
