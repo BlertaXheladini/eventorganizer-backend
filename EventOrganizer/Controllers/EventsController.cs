3747d5236e53ce3148abdb011e50c8d8432a9335
BlertaXheladini <blerta.xheladini03@gmail.com>
2025-04-29 15:57:16 +0200

    Created Search and Order methods on Events Controller

--- EventOrganizer/Controllers/EventsController.cs
+++ EventOrganizer/Controllers/EventsController.cs
@@ -120,5 +120,45 @@ namespace OrganizingEvents.Controllers
             await _db.SaveChangesAsync();
             return NoContent();
         }
+
+
+        // Search Events by Name
+        [HttpGet("SearchEvent")]
+        public async Task<IActionResult> SearchEventAsync([FromQuery] string searchEvent)
+        {
+            if (string.IsNullOrEmpty(searchEvent))
+            {
+                return BadRequest("Search term cannot be empty.");
+            }
+
+            var events = await _db.Events
+                .Include(e => e.EventThemes)
+                .Include(e => e.EventCategories)
+                .Where(e => EF.Functions.Like(e.EventName, $"%{searchEvent}%"))
+                .ToListAsync();
+
+            return Ok(events);
+        }
+
+
+        //Order Events by Price
+        [HttpGet("SortEvents")]
+        public async Task<IActionResult> SortEvents(string sortOrder)
+        {
+            var events = await _db.Events.ToListAsync();
+
+            if (sortOrder == "LowToHigh")
+            {
+                events = events.OrderBy(e => e.Price).ToList();
+            }
+            else if (sortOrder == "HighToLow")
+            {
+                events = events.OrderByDescending(e => e.Price).ToList();
+            }
+
+            return Ok(events);
+        }
+
+
     }
 }
